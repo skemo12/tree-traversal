c2c55071569544d3d82e7caf2c3a554fb4d0008d
Darius Alexandru Paunoiu <alexdarius2012@yahoo.com>
2023-05-03 19:21:03 +0300

    No more spam

--- test_tree.py
+++ test_tree.py
@@ -15,5 +15,16 @@ class Test_tree(unittest.TestCase):
         self.assertEqual(t.find(5).data, 5)
         self.assertEqual(t.find(6), None)
 
+    def test_tree_find_2(self):
+        t = Tree()
+        for i in range(10):
+            t.add(i)
+        self.assertEqual(t.find(1).data, 1)
+        self.assertEqual(t.find(2).data, 2)
+        self.assertEqual(t.find(3).data, 3)
+        self.assertEqual(t.find(4).data, 4)
+        self.assertEqual(t.find(5).data, 5)
+        self.assertEqual(t.find(11), None)
+
 if __name__ == '__main__':
 	unittest.main()
